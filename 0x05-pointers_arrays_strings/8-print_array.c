fe54bd364c01f262f3cebbc92ff858ad2c8e71ca
imane <imanezenasnii@gmail.com>
2023-06-26 14:06:39 -0700

    add function that prints elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
- /**
+/**
  * print_array -  prints n elements of an array of integers
  * @a: array name
  * @n: number of elements of the array
@@ -8,14 +8,16 @@
 
 void print_array(int *a, int n)
 {
-        for (n = 0; n <= 4; n++)
-        {
-                printf("%d", a[n]);
-                if (n == 4)
-                {
-                        printf("\n");
-                        continue;
-                }
-                printf(", ");
-        }
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i == n - 1)
+		{
+			printf("\n");
+			continue;
+		}
+		printf(", ");
+	}
 }
