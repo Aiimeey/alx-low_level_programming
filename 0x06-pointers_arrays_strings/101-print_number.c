d7859303427c22d6e8eb7f55ebb7391d6cdfcccc
imane <imanezenasnii@gmail.com>
2023-07-01 06:38:37 -0700

    add function that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: number to be print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int j = 1, k, r;
+
+	if (n < 0)
+	{
+		_putchar ('-');
+		n = -n;
+	}
+	k = n;
+
+	while (k >= 10)
+	{
+		k = k / 10;
+		j = j * 10;
+	}
+
+	k = n;
+	while (j >= 1)
+	{
+		k = n / j;
+		r = k % 10;
+		_putchar (r + '0');
+		j = j / 10;
+	}
+
+}
