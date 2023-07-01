f3c90fbb02bcf2c77e17db32ee7b4480db233db6
imane <imanezenasnii@gmail.com>
2023-07-01 10:54:17 -0700

    advanced task

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: string number to add
+ * @n2: string 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)i
+{
+}
