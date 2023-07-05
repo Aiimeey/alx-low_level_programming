c321d96a6da6632e1a4e71b271aa6ad79971cf84
imane <imanezenasnii@gmail.com>
2023-07-05 08:50:15 -0700

    fix a betty style error

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,14 +3,16 @@
  * _strlen_recursion - returns the length of a string
  * @s: string
  * Return: lenght
- */ 
+ */
 int _strlen_recursion(char *s)
-{ int l=0;
+{
+	int l = 0;
 
 	if (*s)
-	{   l++;    
-		l+=_strlen_recursion(s + 1);
+	{
+		l++;
+		l += _strlen_recursion(s + 1);
 	}
-	return(l);
+	return (l);
 
 }
