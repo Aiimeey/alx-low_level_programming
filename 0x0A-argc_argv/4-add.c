9e0cf54076627978a03e74ebb5cd191ad96de755
imane <imanezenasnii@gmail.com>
2023-07-07 14:25:23 -0700

    Implemented program to add positive numbers with error handling

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] >= '0' && *argv[i] <= '9')
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
