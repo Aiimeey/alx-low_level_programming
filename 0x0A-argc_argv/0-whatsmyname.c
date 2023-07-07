6ec8110ab40440645e5aec4f4d0c170f2eb0acb5
imane <imanezenasnii@gmail.com>
2023-07-07 12:47:15 -0700

    Implemented program to print executable name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - Entry point of the program
  * @argc: The number of command-line arguments
@@ -9,7 +8,7 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
