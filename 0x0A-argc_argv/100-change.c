ac405972967d6af92df86aca7aafa48c6a055090
imane <imanezenasnii@gmail.com>
2023-07-08 09:31:19 -0700

    Add C code for converting amount to coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,28 +7,43 @@
  *
  * Return: Always 0 (Success)
  */
-
-
 int main(int argc, char *argv[])
 {
-	int i;
+	int c = 0, num;
 
-	if (argc == 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	for (i = 1; i < argc; i++)
+	num = atoi(argv[1]);
+	if (num < 0)
 	{
-
-		if (argv[i][0] == '-')
-		{
-			printf("0\n");
-			return (0);
-		}
-
-
-}
+		printf("0\n");
 		return (0);
+	}
+	if (num >= 25)
+	{
+		c = num / 25;
+		num = num % 25;
+	}
+	if (num >= 10)
+	{
+		c += num / 10;
+		num = num % 10;
+	}
+	if (num >= 5)
+	{
+		c += num / 5;
+		num = num % 5;
+	}
+	if (num >= 2)
+	{
+		c += num / 2;
+		num = num % 2;
+	}
+	if (num ==  1)
+		c++;
+	printf("%d\n", c);
+	return (0);
 }
