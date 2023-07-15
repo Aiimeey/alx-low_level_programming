89ab780985b4c88daf360aded73e7a1da44c4f70
imane <imanezenasnii@gmail.com>
2023-07-15 10:04:15 -0700

    Advanced task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: number of command-line arguments passed to the program
+ * @argv: array of strings that holds the command-line arguments
+ *
+ * Return: The product of num1 and num2
+ */
+
+
+int main(argc, *argv[])
+{
+	char mul, num1, num2;
+
+	printf("%d\n", mul);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (num1 < 48 && num1 > 57)
+		printf("Error\n");
+	exit(98);
+
+	if (num2 < 48 && num2 > 57)
+		printf("Error\n");
+	exit(98);
+
+
+
+}
