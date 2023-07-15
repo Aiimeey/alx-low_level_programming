192cef77b8c54d86f2536db0ba9eb3d8f94f59f7
imane <imanezenasnii@gmail.com>
2023-07-15 10:43:41 -0700

    Advanced task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - entry point
  * @argc: number of command-line arguments passed to the program
@@ -8,25 +10,29 @@
  */
 
 
-int main(argc, *argv[])
+int main(int argc, char* argv[])
 {
-	char mul, num1, num2;
-
-	printf("%d\n", mul);
+	int  num1, num2, mul;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (num1 < 48 && num1 > 57)
-		printf("Error\n");
-	exit(98);
+	if (*argv[1] < 48 && *argv[1] > 57)
+	{	printf("Error\n");
+		exit(98);
+	}
 
-	if (num2 < 48 && num2 > 57)
-		printf("Error\n");
-	exit(98);
+	if (*argv[2] < 48 && *argv[2] > 57)
+	{		printf("Error\n");
+		exit(98);
+	}	
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
 
-
+	printf("%d",mul);
+	return (0);
 }
