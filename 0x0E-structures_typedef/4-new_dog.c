6fbecfaffe1ca4e61b93e6e47dadf5d63184a082
imane <imanezenasnii@gmail.com>
2023-07-17 07:22:53 -0700

    Structure, typedef project

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - Creates a new instance of a dog structure.
+ * @name: Pointer to a string representing the dog's name.
+ * @age: Float value representing the dog's age.
+ * @owner: Pointer to a string representing the dog's owner.
+ *
+ * Return: Pointer to the newly created dog structure, or NULL if malloc fails.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
@@ -9,11 +18,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog == NULL)
 		return (NULL);
 
+        if(name == NULL)
+                my_dog->name = NULL;
+        else
+{
+                my_dog->name = malloc(strlen(name)+1);
+        if(my_dog->name ==NULL)
+        {
+                free(my_dog);
+                return NULL;
+        }
+        my_dog->name = strcpy(my_dog->name, name);
+}
+        if(owner == NULL)
+                my_dog->owner = NULL;
+        else
+{
+                my_dog->owner = malloc(strlen(owner)+1);
+        if(my_dog->owner ==NULL)
+        {
+                free(my_dog);
+                return NULL;
+        }
+        my_dog->name = strcpy(my_dog->name, name);
+}
 	my_dog->name = name;
 	my_dog->age = age;
 	my_dog->owner = owner;
-
-
 
 	return (my_dog);
 
