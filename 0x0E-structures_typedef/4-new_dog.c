dcd98ae81a1dac55bc0840c8dcbd7724d55bbf62
imane <imanezenasnii@gmail.com>
2023-07-17 07:33:18 -0700

    Structure, typedef project

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,34 +18,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog == NULL)
 		return (NULL);
 
-        if(name == NULL)
-                my_dog->name = NULL;
-        else
-{
-                my_dog->name = malloc(strlen(name)+1);
-        if(my_dog->name ==NULL)
-        {
-                free(my_dog);
-                return NULL;
-        }
-        my_dog->name = strcpy(my_dog->name, name);
-}
-        if(owner == NULL)
-                my_dog->owner = NULL;
-        else
-{
-                my_dog->owner = malloc(strlen(owner)+1);
-        if(my_dog->owner ==NULL)
-        {
-                free(my_dog);
-                return NULL;
-        }
-        my_dog->name = strcpy(my_dog->name, name);
-}
-	my_dog->name = name;
-	my_dog->age = age;
-	my_dog->owner = owner;
+	{
+		my_dog->name = malloc(strlen(name)+1);
+		if(my_dog->name ==NULL)
+		{
+			free(my_dog);
+			return NULL;
+		}
+		my_dog->name = strcpy(my_dog->name, name);
+	}
+	my_dog->owner = malloc(strlen(owner)+1);
+	if(my_dog->owner ==NULL)
+	{
+		free(my_dog);
+		free(my_dog->name);
+		return NULL;
+	}
+	my_dog->name = strcpy(my_dog->name, name);
 
-	return (my_dog);
+
+my_dog->age = age;
+
+return (my_dog);
 
 }
