14ba6b66f879e6daa1e31f3909da766b9f52c840
imane <imanezenasnii@gmail.com>
2023-07-17 07:58:55 -0700

    Structure, typedef project

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,9 +1,10 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * free_dog - Frees memory allocated for a dog structure
  * @d: Pointer to a dog structure
  */
-void free_dog(dog_t *d);
+void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
