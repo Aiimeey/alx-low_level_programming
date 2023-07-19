325f84eb0b864b76092a613a5763671d6ca6268e
imane <imanezenasnii@gmail.com>
2023-07-19 15:58:00 -0700

    add program that performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef calc_h
+#define calc_h
 
 /**
  * struct op - Struct op
@@ -20,5 +20,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 int (*get_op_func(char *s))(int, int);
-
 #endif
