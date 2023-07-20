0c2ccc5ba2cd3fb45457a96798aa6eb59ba4bdb2
imane <imanezenasnii@gmail.com>
2023-07-20 15:05:16 -0700

    Implement a function for variable format printing

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,7 +10,7 @@ void print_all(const char * const format, ...)
 {
 	int j = 0;
 	const char *f;
-	char *p = "";
+	char *p = "", *s;
 
 	va_list(arg);
 
@@ -30,18 +30,18 @@ void print_all(const char * const format, ...)
 			printf("%s%f", p, va_arg(arg, double));
 			break;
 			case's':
-			printf("%s%s", p, va_arg(arg, char *));
+			s = va_arg(arg, char *);
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s%s", p, s);
 			break;
 			default:
 			j++;
 			continue;
 		}
-
 		p = ", ";
-
 		j++;
 	}
-
 	va_end(arg);
 	printf("\n");
 }
