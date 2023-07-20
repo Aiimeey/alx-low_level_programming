78e41cf19282c4801d46b24d2cc8c14f16f61662
imane <imanezenasnii@gmail.com>
2023-07-20 14:44:08 -0700

    Implement a function for variable format printing

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,11 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * print_all - print a number of values based on a given format
+ * @format: the format indicating the data types to print
+ * @...: the list of values to print based on the format
+ */ 
 void print_all(const char * const format, ...)
 {
 	int j = 0;
@@ -11,24 +16,25 @@ void print_all(const char * const format, ...)
 
 	va_start(arg, format);
 
-	while(format && format[j])
+	while (format && format[j])
 	{
 		f = format + j;
-		switch(*f)
-		{   case'c': 
-			printf("%s%c",p,va_arg(arg, int)); 
+		switch (*f)
+		{   case'c':
+			printf("%s%c", p, va_arg(arg, int));
 			break;
-			case'i': 
-			printf("%s%d",p,va_arg(arg, int)); 
+			case'i':
+			printf("%s%d", p, va_arg(arg, int));
 			break;
-			case'f': 
-			printf("%s%f",p,va_arg(arg, double)); 
+			case'f':
+			printf("%s%f", p, va_arg(arg, double));
 			break;
-			case's': 
-			printf("%s%s",p,va_arg(arg, char *)); 
+			case's':
+			printf("%s%s", p, va_arg(arg, char *));
 			break;
-			default: j++;
-				 continue;
+			default:
+			j++;
+			continue;
 		}
 
 		p = ", ";
@@ -39,4 +45,3 @@ void print_all(const char * const format, ...)
 	va_end(arg);
 	printf("\n");
 }
-
