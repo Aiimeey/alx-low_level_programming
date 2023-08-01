e58eba7f88897d5d58545a29bf8156810cf81a08
imane <imanezenasnii@gmail.com>
2023-08-01 04:07:47 -0700

    add a function that frees a linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - Frees a linked list of integers and sets the head to NULL
+ *
+ * @head: pointer to the pointer to the head node of the linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+
+	while (*head)
+	{
+
+		temp = temp->next;
+
+		free(*head);
+		*head = temp;
+	}
+}
