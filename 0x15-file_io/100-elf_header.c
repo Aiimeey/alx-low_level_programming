7691103da6dae7f600e908c26038e405deda3b2a
imane <imanezenasnii@gmail.com>
2023-08-07 13:26:52 -0700

    add program that displays the information contained in the ELF

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -9,5 +9,12 @@
 int main(int argc, char *argv[])
 {
 	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	if (argc != 2)
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
+
+	int fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]), exit(98);
+
 	return (1);
 }
