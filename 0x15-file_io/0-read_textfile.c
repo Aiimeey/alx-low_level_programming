3d60c8e43c853b424b6d675a7f3bb41d2a550c5d
imane <imanezenasnii@gmail.com>
2023-08-07 04:25:35 -0700

    add function that reads a text file and prints it to the standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+/**
+ * read_textfile - Reads characters from a file and writes them to STDOUT
+ * @filename: The name of the file to read
+ * @letters: The number of characters to read from the file
+ *
+ * Return: On success, the number of characters written to STDOUT is returned
+ *         On failure or error, the function returns 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t s_write, s_read;
