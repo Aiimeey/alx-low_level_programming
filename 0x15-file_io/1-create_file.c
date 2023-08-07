5360f61b37a6d391c0c1c87c1996c98a6ac8e864
imane <imanezenasnii@gmail.com>
2023-08-07 04:36:17 -0700

    add function that appends text at the end of a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdio.h>
+/**
+ * create_file - Creates a new file and writes content to it
+ *				 or overwrites it if already exists
+ * @filename: The name of the file to create or overwrite.
+ * @text_content: The content to be written to the file.
+ *
+ * Return: On success, the function returns 1.
+ *         On failure, the function returns -1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t s_print;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+	else if (text_content != NULL)
+	{
+		s_print = dprintf(fd, "%s\n", text_content);
+
+		if (s_print < 0)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
