22e10e7d8702d4cb45ef8343a9748c592a4d1209
imane <imanezenasnii@gmail.com>
2023-09-14 22:25:07 +0100

    Add function to calculate the length of a doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,14 @@
 #include "lists.h"
-
+/**
+ * dlistint_len - Computes the length of a doubly-linked list
+ *
+ * @h: A pointer to the head of the doubly-linked list
+ *
+ * Return: The number of nodes in the list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
 	while (h)
 	{
