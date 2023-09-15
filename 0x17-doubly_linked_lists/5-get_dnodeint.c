164ad5297c8089c3a9156851dff131ac876c2f0b
imane <imanezenasnii@gmail.com>
2023-09-15 01:44:07 +0100

    Implement calculating the sum of elements in the list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,15 +1,21 @@
 #include "lists.h"
-
+/**
+ * get_dnodeint_at_index - Returns a pointer to the node at a specified index
+ * @head: A pointer to the head of the doubly linked list
+ * @index: The index of the node to retrieve 
+ *
+ * Return: A pointer to the node at the specified index, or NULL if not found
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    dlistint_t *temp = head;
-    unsigned int i;
+	dlistint_t *temp = head;
+	unsigned int i;
 
-    for (i = 0; i < index ; i++)
-    {
-        if (temp == NULL)
-            return NULL;
-        temp = temp->next;
-    }
-    return(temp);
+	for (i = 0; i < index ; i++)
+	{
+		if (temp == NULL)
+			return (NULL);
+		temp = temp->next;
+	}
+	return (temp);
 }
