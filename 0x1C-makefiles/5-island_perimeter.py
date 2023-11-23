324f6c3b9e21fd7ae1cbfe314576d2afe1a9592b
imane <imanezenasnii@gmail.com>
2023-11-23 03:41:39 +0100

    Add the island_perimeter in Python to calculate the perimeter of an island in a given grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,5 +3,22 @@
 
 
 def island_perimeter(grid):
-    """island_perimeter Method"""
-    return
+    """island_ma Method"""
+
+    perim = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perim += 4
+
+                if grid[i][j - 1] == 1 and j > 0:
+                    perim -= 1
+                if grid[i - 1][j] == 1 and i > 0:
+                    perim -= 1
+                if grid[i][j + 1] == 1 and j < len(grid[i]) - 1:
+                    perim -= 1
+                if grid[i + 1][j] == 1 and i < len(grid) - 1:
+                    perim -= 1
+
+    return perim
